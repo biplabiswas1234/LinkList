553cb8dcb5ce3003cccb7827319ef7974fd59a5b
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-23 22:05:15 +0530

    [Biplab] Add: Simple List

---
+++ ConsoleApp1/LinkListClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class LinkListClass
+    {
+       List<int>  Number = new List<int>();
+        public void Write()
+        {
+            Number.Add(56);
+            Number.Add(30);
+            Number.Add(70);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Numbers are:");
+            foreach (int i in Number)
+            {
+               Console.WriteLine(i);
+            }
+        }
+        
+    }
+}
